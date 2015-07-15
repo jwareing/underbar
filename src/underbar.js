48657e6f0eda90607eb694eeb325c1d39c8471a7
John Wareing <jwareing89@gmail.com>
2015-07-15 13:02:27 -0500

    Fixed _.filter function, returns only array of values that pass truth test.

--- src/underbar.js
+++ src/underbar.js
@@ -87,6 +87,13 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var passed = [];
+    _.each(collection,function(item){
+      if (test(item)==true){
+        passed.push(item);
+      };
+    });
+    return passed;
   };
 
   // Return all elements of an array that don't pass a truth test.
