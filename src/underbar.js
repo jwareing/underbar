83d772f2fee22c1b5c78c6232109cac49841e82b
John Wareing <jwareing89@gmail.com>
2015-07-15 15:45:01 -0500

    Fixed _.map function to transform array elements and create new array. Used _.each in solution.

--- src/underbar.js
+++ src/underbar.js
@@ -124,6 +124,11 @@
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+    var newArr = [];
+    _.each(collection, function(item){
+      newArr.push(iterator(item));
+    });
+    return newArr
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
