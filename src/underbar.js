7614a38a6e79ce501f57dd38f1b5990d39354d37
John Wareing <jwareing89@gmail.com>
2015-07-22 09:53:47 -0500

    Fixed _.extend function to add properties of objects to first argument.

--- src/underbar.js
+++ src/underbar.js
@@ -241,6 +241,15 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var extendedObj = arguments[0];
+    for (var i=1; i<arguments.length; i++){
+      var currentObj=arguments[i]
+      for (var prop in currentObj){
+        var currentVal=currentObj[prop];
+        extendedObj[prop]=currentVal;
+      }
+    }
+    return extendedObj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
